e9a5e2fa34a6f3d48b3beea591b4f254ffbce3d8
Darklline <ivaxy12@gmail.com>
2020-04-27 18:43:50 +0200

    Add project files.

---
+++ LibraryApp.Data/Entities/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.Data.Entities
+{
+    public class Author
+    {
+        public Author()
+        {
+
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Book> Books { get; set; }
+
+    }
+}
